bf87f1f5edee5408824bf30ccd7fa6121b6d0236
Mayran <loicmayran@gmail.com>
2024-11-22 14:36:52 +0100

    TP4 : Fin + Utilisation Postman

--- src/main/java/fr/diginamic/spring/VilleControleur.java
+++ src/main/java/fr/diginamic/spring/VilleControleur.java
@@ -1,29 +1,42 @@
 package fr.diginamic.spring;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping("villes")
 public class VilleControleur {
+    List<Ville> villes = new ArrayList<>(
+            Arrays.asList(
+                    new Ville("Paris", "2 165 000"),
+                    new Ville("Lyon", "515 695"),
+                    new Ville("Marseille", "861 635"),
+                    new Ville("Toulouse", "493 465"),
+                    new Ville("Nice", "343 000"),
+                    new Ville("Nantes", "314 138"),
+                    new Ville("Strasbourg", "280 966"),
+                    new Ville("Bordeaux", "257 068"),
+                    new Ville("Lille", "233 000"),
+                    new Ville("Montpellier", "290 000")));
 
     @GetMapping
     public List<Ville> getVilles() {
-        List<Ville> villes = new ArrayList<>();
-        villes.add(new Ville("Paris", "2 165 000"));
-        villes.add(new Ville("Lyon", "515 695"));
-        villes.add(new Ville("Marseille", "861 635"));
-        villes.add(new Ville("Toulouse", "493 465"));
-        villes.add(new Ville("Nice", "343 000"));
-        villes.add(new Ville("Nantes", "314 138"));
-        villes.add(new Ville("Strasbourg", "280 966"));
-        villes.add(new Ville("Bordeaux", "257 068"));
-        villes.add(new Ville("Lille", "233 000"));
-        villes.add(new Ville("Montpellier", "290 000"));
         return villes;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> addVille(@RequestBody Ville ville) {
+        for(Ville v : villes){
+            if(v.getNom().equals(ville.getNom())){
+                return new ResponseEntity<>("La ville existe déjà", HttpStatus.BAD_REQUEST); // Code 400
+            }
+        }
+        villes.add(ville);
+        return new ResponseEntity<>("Ville insérée avec succès", HttpStatus.OK);
     }
 }
