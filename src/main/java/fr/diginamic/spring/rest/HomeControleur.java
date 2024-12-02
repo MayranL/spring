eae0c8ec383ec6e5b8166f630717654109a29a4b
Mayran <loicmayran@gmail.com>
2024-12-02 17:13:36 +0100

    [MVC] : Ajout d'une vue MVC avec un index.html qui permet de gérer les requêtes pour les villes et département

---
+++ src/main/java/fr/diginamic/spring/rest/HomeControleur.java
@@ -0,0 +1,27 @@
+package fr.diginamic.spring.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class HomeControleur {
+
+    // Route pour afficher la page d'accueil
+    @GetMapping("/")
+    public String showHomePage() {
+        return "index";  // Affiche index.html
+    }
+
+    // Route pour afficher la page intermédiaire de villes
+    @GetMapping("/ville")
+    public String showVillePage() {
+        return "ville";  // Affiche ville.html, une page avec des actions liées aux villes
+    }
+
+    // Route pour afficher la page des départements
+    @GetMapping("/departement")
+    public String showDepartements() {
+        return "departement";  // Affiche departement.html
+    }
+}
